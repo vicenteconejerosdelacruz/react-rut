27cb68ff121ea54e1e9dda2f2c46262861e2c673
Vicente Conejeros <vichoconejeros@users.noreply.github.com>
2021-02-22 21:09:42 -0300

    release 1.0.0

--- Rut.js
+++ Rut.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 /*
 Author: @vichoconejeros
 
 How to use this
 
-<Rut value={this.state.rut} onChange={this.onRutChange} onValid={this.onRutValido} >
+on the web
+<Rut value={rut} onChange={onRutChange} onValid={onRutValido} >
 	<input type="text" name="rut" placeholder="Rut" required></input>
+</Rut>
+
+on react native
+<Rut value={rut} onChangeText={onRutChange} onValid={onRutValido} >
+	<TextInput placeholder="Rut"></TextInput>
 </Rut>
 
 */
 
-class Rut extends Component {
+const Rut = ({value,onChange,onChangeText,onValid,children}) => {
 	
-	formato = (rut) => {
+	const formato = (rut) => {
 		if (!rut | typeof rut !== 'string') return false
 		return /^\d{1,2}.\d{3}.\d{3}-[k|K|\d]{1}$/.test(rut)
 	}
 	
-	digitoVerificador = (rut) => {
+	const digitoVerificador = (rut) => {
 		let multiplos = [2,3,4,5,6,7]
 		let digitos = rut.split('-')[0].replace(/\./g,'').split('').reverse()
 		let digitoVerificador = rut.split('-')[1].toUpperCase()
 		let digito = 11 - digitos.reduce((acc,elem,index)=>(acc+Number(elem)*multiplos[index%multiplos.length]),0)%11
-		let digimap = [NaN,'1','2','3','4','5','6','7','8','9','K','0']	
+		let digimap = [NaN,'1','2','3','4','5','6','7','8','9','K','0']
 		return digimap[digito]===digitoVerificador
 	}
 
 	 
-	rutValido = (rut) => {
-		return this.formato(rut) && this.digitoVerificador(rut)
+	const rutValido = (rut) => {
+		return formato(rut) && digitoVerificador(rut)
 	}
 		
-	reformat = (rutViejo,rutNuevo) => {
+	const reformat = (rutViejo,rutNuevo) => {
 		let digitos = ['0','1','2','3','4','5','6','7','8','9','K','k']
 		let digitoValido = (digito) => digitos.includes(digito)			
 		let quitarDV = (digito,index,array) => (index<(array.length-1))?!(['K','k'].includes(digito)):true
@@ -40,6 +46,7 @@ class Rut extends Component {
 		if(rutNuevo !==''){
 			
 			let chars = rutNuevo.split('').filter(digitoValido).filter(quitarDV)
+			if(chars.length>9) return rutViejo
 			let digito = chars[chars.length-1]
 			
 			if(digitoValido(digito)){
@@ -52,28 +59,22 @@ class Rut extends Component {
 		return '';
 	}	
 	
-	onChange = (e) => {
-		e.target.value = this.reformat(this.props.value,e.target.value)
-		this.props.onChange(e)
-		if(this.props.onValid) this.props.onValid(this.rutValido(e.target.value))
+	const onChangeValue = (e) => {
+		e.target.value = reformat(value,e.target.value)
+		onChange(e)
+		if(onValid) onValid(rutValido(e.target.value))
 	}
 	
-	
-	renderChildren = () =>{
-	  return React.Children.map(this.props.children, (child, i)=>{
-		return React.cloneElement(child,{
-		  value:this.props.value,
-		  onChange:this.onChange
-		})
-	  })
+	const onChangeTextValue = (rut) => {
+		rut = reformat(value,rut)
+		onChangeText(rut)
+		if(onValid) { onValid(rutValido(rut)) }
 	}
 	
-	render(){
-		return (
-			<div>
-				{this.renderChildren()}
-			</div>
-		)
+	if (typeof document !== 'undefined') {
+		return React.Children.map(children, (child, i)=> React.cloneElement(child,{value:value,onChange:onChangeValue}))
+	} else if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
+		return React.Children.map(children, (child, i)=> React.cloneElement(child,{value:value,onChangeText:onChangeTextValue}))
 	}
 }
 
